a870d6377fe2f75c906cf03e4411edde510bb4e0
Daffarel Adyatma Winuradmaja <farelalden96@gmail.com>
2023-11-13 14:55:15 +0700

    add pop up for donasi

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,14 @@
-// script.js
-document.getElementById('showPopup').addEventListener('click', function () {
-    document.getElementById('popupContainer').style.display = 'block';
+function togglePopup(popupId) {
+    document.body.classList.add('popup-open');
+    document.getElementById(popupId).style.display = 'block';
+}
+document.getElementById('showPopup1').addEventListener('click', function () {
+    togglePopup('popupContainer1');
 });
-
 // Menutup pop-up jika pengguna mengklik di luar kontennya
 window.addEventListener('click', function (event) {
-    if (event.target === document.getElementById('popupContainer')) {
-        document.getElementById('popupContainer').style.display = 'none';
+    if (event.target.classList.contains('popup')) {
+        document.body.classList.remove('popup-open');
+        event.target.style.display = 'none';
     }
 });
