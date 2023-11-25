c0aed2f40f1df27ec7945992c72ce509b0469761
alka <mr.adikna@gmail.com>
2023-11-25 17:47:56 +0700

    extend popup done

--- js/pengembalian.js
+++ js/pengembalian.js
@@ -1,9 +1,28 @@
+function togglePopup(index) {
+    // switch (index) {
+    //     case 1:
+    //         document.getElementById("extend").classList.toggle("active");
+    //     case 2:
+    //         document.getElementById("extend").classList.remove("active");
+    //         document.getElementById("return").classList.toggle("active");
+    //     default:
 
-function extend() {
-    document.getElementById("extend").classList.toggle("active");
+    // }
+
+    if(index == 1) {
+        document.getElementById("extend").classList.toggle("active");
+    }
+
+    if(index == 2) {
+        document.getElementById("extend").classList.remove("active");
+        document.getElementById("extendconfirm").classList.toggle("active");
+    }
+
+
 }
 
 
-function returnn() {
-    popup.classList.toggle("active");
-}
+// function returnn() {
+//     document.getElementById("extend").classList.remove("active");
+//     document.getElementById("return").classList.toggle("active");
+// }
